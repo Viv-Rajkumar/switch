8705abaa61273ae3577199ce123a02a841f7720e
Viv-Rajkumar <viv.rajkumar@maidsafe.net>
2014-09-01 10:27:08 +0100

    Add support for header/implementation files in different folders.

--- Switch/SwitchCore/SwitchHelper.cs
+++ Switch/SwitchCore/SwitchHelper.cs
@@ -1,54 +1,66 @@
 ﻿using System;
-using EnvDTE;
 using System.IO;
+using System.Linq;
+using EnvDTE;
 using EnvDTE80;
 
-namespace SwitchCore
-{
+namespace SwitchCore {
+  /// <summary>
+  ///   Helper functions for DoSwitch functionality.
+  /// </summary>
+  public static class SwitchHelper {
     /// <summary>
-    /// Helper functions for DoSwitch functionality.
+    ///   Tries the open document.
     /// </summary>
-    public static class SwitchHelper
-    {
-        /// <summary>
-        /// Tries the open document.
-        /// </summary>
-        /// <param name="application">The application.</param>
-        /// <param name="path">The path.</param>
-        /// <returns></returns>
-        public static bool TryOpenDocument(DTE2 application, string path)
-        {
-            //  Go through each document in the solution.
-            foreach(Document document in application.Documents)
-            {
-                if(String.CompareOrdinal(document.FullName, path) == 0)
-                {
-                    //  The document is open, we just need to activate it.
-                    if (document.Windows.Count > 0)
-                    {
-                        document.Activate();
-                        return true;
-                    }
-                }
-            }
-
-            //  The document isn't open - does it exist?
-            if (File.Exists(path))
-            {
-                try
-                {
-                    application.Documents.Open(path, "Text", false);
-                    return true;
-                }
-                catch (Exception)
-                {
-                    //  We can't open the doc.
-                    return false;
-                }
-            }
-
-            //  We couldn't open the document.
-            return false;
+    /// <param name="application">The application.</param>
+    /// <param name="path">The path.</param>
+    /// <returns></returns>
+    public static bool TryOpenDocument(DTE2 application, string path) {
+      //  Go through each document in the solution.
+      foreach (Document document in application.Documents) {
+        if (String.CompareOrdinal(document.FullName, path) == 0) {
+          //  The document is open, we just need to activate it.
+          if (document.Windows.Count > 0) {
+            document.Activate();
+            return true;
+          }
         }
+      }
+
+      //  The document isn't open - does it exist?
+      if (File.Exists(path)) {
+        try {
+          application.Documents.Open(path, "Text", false);
+          return true;
+        } catch (Exception) {
+          //  We can't open the doc.
+          return false;
+        }
+      }
+
+      //  We couldn't open the document.
+      return false;
     }
+
+    public static bool TryOpenDocumentFromProjectIncludes(DTE2 application, string path) {
+      foreach (var document in from Document document in application.Documents
+        where String.CompareOrdinal(document.FullName, path) == 0
+        where document.Windows.Count > 0
+        select document) {
+        document.Activate();
+        return true;
+      }
+
+      if (!File.Exists(path)) {
+        return false;
+      }
+
+      try {
+        application.Documents.Open(path, "Text", false);
+        return true;
+      } catch (Exception) {
+        return false;
+      }
+    }
+  }
 }
--- Switch/SwitchCore/SwitchTargets/ExtensionSwitchTarget.cs
+++ Switch/SwitchCore/SwitchTargets/ExtensionSwitchTarget.cs
@@ -1,82 +1,121 @@
-﻿using EnvDTE;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using EnvDTE;
 using EnvDTE80;
+using Microsoft.VisualStudio.VCCodeModel;
+using Microsoft.VisualStudio.VCProjectEngine;
 
-namespace SwitchCore.SwitchTargets
-{
+namespace SwitchCore.SwitchTargets {
+  /// <summary>
+  ///   An Extension DoSwitch Target switches files based on extension.
+  /// </summary>
+  public class ExtensionSwitchTarget : ISwitchTarget {
     /// <summary>
-    /// An Extension DoSwitch Target switches files based on extension.
+    ///   Initializes a new instance of the <see cref="ExtensionSwitchTarget" /> class.
     /// </summary>
-    public class ExtensionSwitchTarget : ISwitchTarget
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ExtensionSwitchTarget"/> class.
-        /// </summary>
-        /// <param name="from">From.</param>
-        /// <param name="to">To.</param>
-        public ExtensionSwitchTarget(string from, string to)
-        {
-            From = from;
-            To = to;
-        }
-        
-        /// <summary>
-        /// Switches the specified document.
-        /// </summary>
-        /// <param name="application">The application.</param>
-        /// <param name="activeDocument">The active document.</param>
-        /// <returns>
-        /// True if switched successfully.
-        /// </returns>
-        public bool DoSwitch(DTE2 application, Document activeDocument)
-        {
-            //  Get the doc path.
-            var path = activeDocument.FullName;
-
-            //  If there's no from path then we can't continue.
-            if (path.EndsWith(From) == false)
-                return false;
-
-            //  Replace the path 'from' with the part 'to'.
-            if (path.Length < From.Length)
-                return false;
-
-            //  Map the path.
-            var mappedPath = path.Substring(0, path.Length - From.Length) + To;
-
-            //  Try and open the mapped path.
-            return SwitchHelper.TryOpenDocument(application, mappedPath);
-        }
-
-        /// <summary>
-        /// Determines whether this instance can switch given the specified document.
-        /// </summary>
-        /// <param name="application">The application.</param>
-        /// <param name="activeDocument">The active document.</param>
-        /// <returns>
-        ///   <c>true</c> if this instance can switch the specified application; otherwise, <c>false</c>.
-        /// </returns>
-        public bool CanSwitch(DTE2 application, Document activeDocument)
-        {
-            //  For now we always enable the command.
-            return true;
-        }
-
-        /// <summary>
-        /// Gets from.
-        /// </summary>
-        public string From
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// Gets to.
-        /// </summary>
-        public string To
-        {
-            get;
-            private set;
-        }
+    /// <param name="from">From.</param>
+    /// <param name="to">To.</param>
+    public ExtensionSwitchTarget(string from, string to) {
+      From = from;
+      To = to;
     }
+
+    /// <summary>
+    ///   Gets from.
+    /// </summary>
+    public string From { get; private set; }
+
+    /// <summary>
+    ///   Gets to.
+    /// </summary>
+    public string To { get; private set; }
+
+    /// <summary>
+    ///   Switches the specified document.
+    /// </summary>
+    /// <param name="application">The application.</param>
+    /// <param name="activeDocument">The active document.</param>
+    /// <returns>
+    ///   True if switched successfully.
+    /// </returns>
+    public bool DoSwitch(DTE2 application, Document activeDocument) {
+      var folderPath = Path.GetDirectoryName(activeDocument.FullName);
+      var fileName = Path.GetFileNameWithoutExtension(activeDocument.FullName);
+      var extension = Path.GetExtension(activeDocument.FullName);
+
+      Debug.Assert(folderPath != null, "folderPath != null");
+      Debug.Assert(fileName != null, "fileName != null");
+
+      if (extension == null || extension.Replace(".", "") != From) {
+        return false;
+      }
+
+      var mappedPath = Path.Combine(folderPath, fileName) + "." + To;
+      if (SwitchHelper.TryOpenDocument(application, mappedPath)) {
+        return true;
+      }
+
+      try {
+        var currentProject = activeDocument.ProjectItem.ContainingProject;
+        var currentFile = activeDocument.ProjectItem.Object as VCFile;
+
+        if (currentFile.FileType == eFileType.eFileTypeCppCode) {
+          var configName = currentProject.ConfigurationManager.ActiveConfiguration.ConfigurationName;
+          mappedPath = ResolveHeaderPath(activeDocument, fileName, currentFile, configName);
+        } else {
+          mappedPath = ResolveSourcePath(activeDocument, fileName);
+        }
+
+        return SwitchHelper.TryOpenDocumentFromProjectIncludes(application, mappedPath);
+      } catch (Exception) {
+        return false;
+      }
+    }
+
+    /// <summary>
+    ///   Determines whether this instance can switch given the specified document.
+    /// </summary>
+    /// <param name="application">The application.</param>
+    /// <param name="activeDocument">The active document.</param>
+    /// <returns>
+    ///   <c>true</c> if this instance can switch the specified application; otherwise, <c>false</c>.
+    /// </returns>
+    public bool CanSwitch(DTE2 application, Document activeDocument) {
+      //  For now we always enable the command.
+      return true;
+    }
+
+    private string ResolveHeaderPath(Document activeDocument, string fileName, VCFile currentFile, string configName) {
+      var codeModel = activeDocument.ProjectItem.FileCodeModel as VCFileCodeModel;
+      CodeElement codeElement =
+        codeModel.Includes.Cast<CodeElement>().
+          FirstOrDefault(element => element.FullName.EndsWith(fileName + "." + To));
+      var fileConfig = currentFile.FileConfigurations.Item(configName) as VCFileConfiguration;
+      var fileRule = fileConfig.Tool as IVCRulePropertyStorage;
+      var includeDirectories = fileRule.GetEvaluatedPropertyValue("AdditionalIncludeDirectories").Split(';');
+      foreach (
+        var headerPath in
+          includeDirectories.Select(
+            includeDirectory =>
+              Path.Combine(includeDirectory, codeElement.FullName).
+                Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar)).
+            Where(File.Exists)) {
+        return headerPath;
+      }
+
+      throw new FileNotFoundException();
+    }
+
+    private string ResolveSourcePath(Document activeDocument, string fileName) {
+      var currentVCProject = activeDocument.ProjectItem.ContainingProject.Object as VCProject;
+      foreach (VCFile file in currentVCProject.Files) {
+        if (file.FullPath.EndsWith(fileName + "." + To)) {
+          return file.FullPath;
+        }
+      }
+      throw new FileNotFoundException();
+    }
+  }
 }
